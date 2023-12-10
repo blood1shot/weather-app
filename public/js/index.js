f38ae9c5007db7dbafd3af9ec5439d7f40bb6ec2
oleksii.yatsentiuk <oleksii.yatsentiuk@onix-systems.com>
2023-12-10 12:27:11 +0200

    Updated forecast message

--- public/js/index.js
+++ public/js/index.js
@@ -23,13 +23,12 @@ weatherForm.addEventListener("submit", (e) => {
 
   fetch(`/weather?address=${location}`).then((res) => {
     res.json().then((data) => {
-      console.log(data);
       if (data.error) {
         weatherTitle.textContent = "Error";
         weatherError.textContent = data.error;
       } else if (data.current) {
         weatherTitle.textContent = `Weather info in ${data.location.name}`;
-        weatherInfo.textContent = `${data.location.localtime}, ${data.current.weather_descriptions[0]}, feels like ${data.current.feelslike}, wind speed ${data.current.wind_speed}`;
+        weatherInfo.textContent = `${data.location.localtime}, ${data.current.weather_descriptions[0]}. It is currently ${data.current.temperature} degrees out. It feels like ${data.current.feelslike}. The humidity is ${data.current.humidity}%`;
       }
       button.disabled = false;
     });
